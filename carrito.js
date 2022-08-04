5de5cae1a249c0c75dfd1524fbea8287579b20c7
Mariano Aguilar <marianoaguilar2@hotmail.com>
2022-08-04 02:50:19 -0300

    masImplementaciones

--- carrito.js
+++ carrito.js
@@ -86,12 +86,18 @@ function renderItemsCarrito(){
                             <div class="col-md-3 col-lg-2 col-xl-2 offset-lg-1">
                               <h6 class="mb-0">Precio unitario:\n $${item.precio}</h6>
                             </div>
-                            <div class="col-md-1 col-lg-1 col-xl-1 text-end">
-                              <a href="#!" class="text-muted"><i class="fas fa-times"></i></a>
+                            <div class="col-md-1 col-lg-1 col-xl-1 text-end" >
+                              <a href="#!" class="text-muted" onclick= "removerItemsCarrito(${item.id})"><i class="fas fa-times"></i></a>
                             </div>
                           </div>
                           `
     })
+}
+
+// remover items del carrito
+function removerItemsCarrito(id) {
+    carrito = carrito.filter( (item) => item.id !== id);
+    actualizarCarrito();
 }
 
 // funcion cambiar numero de unidades
